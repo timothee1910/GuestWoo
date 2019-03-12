5ee33073193aebbec5b4f369da63eabd8d54e933
Timothée <ong.timothee.cy@gmail.com>
2019-03-13 00:03:32 +0100

    GW-0001: changement produit
    Signed-off-by: Timothée <ong.timothee.cy@gmail.com>

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -42,12 +42,6 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat%7CRoboto:300,400,700" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
-
     @yield('extra-css')
 </head>
 
